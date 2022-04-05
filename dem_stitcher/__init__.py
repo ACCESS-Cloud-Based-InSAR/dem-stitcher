da2e05aae3e0a91c8a5a1113653f4a282d9f7f60
Joseph H Kennedy <me@jhkennedy.org>
2022-04-04 17:10:28 -0800

    provide version number

--- dem_stitcher/__init__.py
+++ dem_stitcher/__init__.py
@@ -1,5 +1,22 @@
+import warnings
+
+# FIXME: Python 3.8+ this should be `from importlib.metadata...`
+from importlib_metadata import PackageNotFoundError, version
+
 from .stitcher import stitch_dem
 
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = None
+    warnings.warn(
+        'package is not installed!\n'
+        'Install in editable/develop mode via (from the top of this repo):\n'
+        '   python -m pip install -e .\n', RuntimeWarning
+    )
 
-__all__ = ['stitch_dem',
-           ]
+
+__all__ = [
+    'stitch_dem',
+    '__version__',
+]
