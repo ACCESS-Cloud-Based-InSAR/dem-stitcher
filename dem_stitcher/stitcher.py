bb00d2b34dc597daa9c07cac1e3c184508064f83
cmarshak <charles.marshak@gmail.com>
2022-05-25 15:37:50 -0700

    lint

--- dem_stitcher/stitcher.py
+++ dem_stitcher/stitcher.py
@@ -193,7 +193,7 @@ def merge_and_transform_dem_tiles(datasets: list,
                                                               resampling='bilinear')
         dem_arr = dem_arr[0, ...]
 
-    ## Ensure dem_arr has correct shape
+    # Ensure dem_arr has correct shape
     assert(len(dem_arr.shape) == 2)
 
     return dem_arr, dem_profile
