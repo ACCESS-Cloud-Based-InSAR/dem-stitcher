3626bbee6876458a2a2197179969a62602a13f36
cmarshak <charles.marshak@gmail.com>
2022-06-14 16:21:59 -0700

    add exceptions

--- dem_stitcher/exceptions.py
+++ dem_stitcher/exceptions.py
@@ -1,2 +1,8 @@
 class NoDEMCoverage(Exception):
+    "Throw this exception if extent/bounds does not cover any available DEM tiles"
+    pass
+
+
+class DEMNotSupported(Exception):
+    "Throw this exception if DEM Name is not supported"
     pass
